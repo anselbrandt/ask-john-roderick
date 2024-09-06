a15ee89576f0baeae65300839599550274e94fac
anselbrandt <ansel@anselbrandt.com>
2024-09-06 04:16:32 -0400

    Add audio-player-context

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
+import { useEffect } from "react";
 import { filelist } from "./filelist";
 import "./App.css";
+import { useAudioPlayerContext, Track } from "./context/audio-player-context";
 
 function App() {
+  const { audioRef, currentTrack, isPlaying, setIsPlaying, setCurrentTrack } =
+    useAudioPlayerContext();
+
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.volume = 50 / 100;
+    }
+  }, [audioRef]);
+
+  useEffect(() => {
+    if (isPlaying) {
+      audioRef.current?.play();
+    } else {
+      audioRef.current?.pause();
+    }
+
+    return () => {};
+  }, [isPlaying, audioRef]);
+
+  const handleClick = (track: Track) => {
+    setCurrentTrack(track);
+    setIsPlaying(true);
+  };
+
   return (
     <>
       <div>
@@ -9,10 +35,23 @@ function App() {
           <h1>Ask John Roderick</h1>
         </a>
       </div>
+      <div>
+        <audio
+          src={`/audio/roadwork/001/${currentTrack.file}`}
+          ref={audioRef}
+          onLoadedMetadata={() => ({})}
+        />
+      </div>
 
       <div className="results">
-        {filelist.map((entry) => (
-          <span className="sentence">{entry.text}</span>
+        {filelist.map((entry, idx) => (
+          <span
+            key={idx}
+            onClick={() => handleClick(entry)}
+            className="sentence"
+          >
+            {entry.text}
+          </span>
         ))}
       </div>
     </>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+import { AudioPlayerProvider } from "./context/audio-player-context.tsx";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AudioPlayerProvider>
+      <App />
+    </AudioPlayerProvider>
+  </StrictMode>
+);

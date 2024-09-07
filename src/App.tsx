3bd2e7ce75a2fecf430175e05b7db395d0382405
anselbrandt <ansel@anselbrandt.com>
2024-09-06 23:54:13 -0400

    Continuous play working

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,79 @@
-import { useEffect } from "react";
+import { useEffect, useCallback } from "react";
 import { filelist } from "./filelist";
 import "./App.css";
 import { useAudioPlayerContext, Track } from "./context/audio-player-context";
 
 function App() {
-  const { audioRef, currentTrack, isPlaying, setIsPlaying, setCurrentTrack } =
-    useAudioPlayerContext();
+  const {
+    currentTrack,
+    audioRef,
+    setDuration,
+    duration,
+    setTimeProgress,
+    progressBarRef,
+    setTrackIndex,
+    setCurrentTrack,
+    isPlaying,
+    setIsPlaying,
+  } = useAudioPlayerContext();
 
-  useEffect(() => {
-    if (audioRef.current) {
-      audioRef.current.volume = 50 / 100;
+  const updateProgress = useCallback(() => {
+    if (audioRef.current && progressBarRef.current && duration) {
+      const currentTime = audioRef.current.currentTime;
+      setTimeProgress(currentTime);
+
+      progressBarRef.current.value = currentTime.toString();
+      progressBarRef.current.style.setProperty(
+        "--range-progress",
+        `${(currentTime / duration) * 100}%`
+      );
     }
-  }, [audioRef]);
+  }, [duration, setTimeProgress, audioRef, progressBarRef]);
 
   useEffect(() => {
     if (isPlaying) {
       audioRef.current?.play();
     } else {
       audioRef.current?.pause();
+      updateProgress(); // Ensure progress is updated immediately when paused
     }
 
     return () => {};
-  }, [isPlaying, audioRef]);
+  }, [isPlaying, updateProgress, audioRef]);
+
+  const onLoadedMetadata = () => {
+    const seconds = audioRef.current?.duration;
+    if (seconds !== undefined) {
+      setDuration(seconds);
+      if (progressBarRef.current) {
+        progressBarRef.current.max = seconds.toString();
+      }
+    }
+  };
+
+  const handleNext = useCallback(() => {
+    setTrackIndex((prev) => {
+      const newIndex = prev >= filelist.length - 1 ? 0 : prev + 1;
+      setCurrentTrack(filelist[newIndex]);
+      return newIndex;
+    });
+  }, [setCurrentTrack, setTrackIndex]);
+
+  useEffect(() => {
+    const currentAudioRef = audioRef.current;
+
+    if (currentAudioRef) {
+      currentAudioRef.onended = () => {
+        handleNext();
+      };
+    }
+
+    return () => {
+      if (currentAudioRef) {
+        currentAudioRef.onended = null;
+      }
+    };
+  }, [handleNext, audioRef]);
 
   const handleClick = (track: Track) => {
     setCurrentTrack(track);
@@ -30,16 +82,21 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://www.johnroderick.com/" className="logo">
-          <h1>Ask John Roderick</h1>
-        </a>
+      <div className="header">
+        <div>
+          <div className="logo">
+            <a href="https://www.johnroderick.com/">
+              <h1>Ask John Roderick</h1>
+            </a>
+          </div>
+          <div className="searchbar">search bar</div>
+        </div>
       </div>
       <div>
         <audio
           src={`/audio/roadwork/001/${currentTrack.file}`}
           ref={audioRef}
-          onLoadedMetadata={() => ({})}
+          onLoadedMetadata={onLoadedMetadata}
         />
       </div>
 
